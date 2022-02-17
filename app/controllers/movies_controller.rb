d650e609f52e81c2ecc917488204dba7d3d2d389
Ibrahim ~Swift <ibrahimq1998@gmail.com>
2022-02-17 15:57:51 +0200

    added a form in slim

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -2,9 +2,10 @@ class MoviesController < ApplicationController
     def index
         @movies = Movie.all
     end 
-
     def show
         id = params[:id]
         @movie = Movie.find(id)
+    end
+    def new
     end
 end
