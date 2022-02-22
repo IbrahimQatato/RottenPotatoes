34a5629df53d06da2b9230fc83f34a807757c8c3
Ibrahim ~7sbeh-build <ibrahimq1998@gmail.com>
2022-02-22 08:12:55 +0200

    updated all Crud actions

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -9,7 +9,36 @@ class MoviesController < ApplicationController
     def new
     end
     def create
-        #write code here
-        
+        if (@movie = Movie.create(movie_params))
+            redirect_to movies_path, :notice => "#{@movie.title} was successfully created."
+        else
+            flas[:alert] = "Movie #{@movie.title} could not be created: "+
+                @movie.errors.full_messages.join(",")
+            render 'new'
+        end
+    end
+
+    def edit
+        @movie = Movie.find params[:id]
+    end
+    
+    def update
+        @movie = Movie.find params[:id]
+        @movie.update_attributes!(movie_params)
+        flash[:notice] = "#{@movie.title} was successfully updated."
+        redirect_to movie_path(@movie)
+    end
+
+    def destroy
+        @movie = Movie.find(params[:id])
+        @movie.destroy
+        flash[:notice] = "Movie '#{@movie.title}' deleted."
+        redirect_to movies_path
+    end
+
+    private
+    def movie_params
+        params.require(:movie)
+        params[:movie].permit(:title, :rating, :release_date)
     end
 end
